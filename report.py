446a1c887856cd7434e92e54ed5d6274166c2fda
a_halka <halka.rjch@gmail.com>
2017-10-11 10:36:14 +0900

    メソッドに分割、waveファイルを再生するメソッドを追加

--- report.py
+++ report.py
@@ -1,38 +1,52 @@
 # -*- coding: UTF-8 -*-
-import sqlite3
 import os
-import requests
-import pygame.mixer
-import time
-from datetime import datetime as dt
 
 # now
-now = dt.now()
-nowdatetime = now.strftime('%Y-%m-%d %H:%M:%S')
+def now():
+    from datetime import datetime as dt
+    now = dt.now()
+    return now.strftime('%Y-%m-%d %H:%M:%S')
 
-# sound
-# It is necessary to change the frequency according to the frequency of the audio file.
-pygame.mixer.init(frequency = 48000)
-# Path to sound file. 
-pygame.mixer.music.load(os.path.abspath(os.path.dirname(__file__))+"/hoge.mp3")
-# Count of play the sound file..
-pygame.mixer.music.play(1)
-# Time to play.
-time.sleep(3)
-pygame.mixer.music.stop()
+def playMp3(path, freq, duration):
+    import pygame.mixer
+    import time
+    # It is necessary to change the frequency according to the frequency of the audio file.
+    pygame.mixer.init(frequency = freq)
+    # Path to sound file. 
+    pygame.mixer.music.load(path)
+    # Count of play the sound file..
+    pygame.mixer.music.play(1)
+    # Time to play.
+    time.sleep(duration)
+    pygame.mixer.music.stop()
+
+def playWave(path):
+    import subprocess
+    subprocess.call('aplay '+path, shell=True)
 
 # db
-con = sqlite3.connect(os.path.abspath(os.path.dirname(__file__))+"/report.db")
-c = con.cursor()
-c.execute("insert into report values (?)",(nowdatetime,))
-con.commit()
-con.close()
+def insertDB(dbfile, message):
+    import sqlite3
+    con = sqlite3.connect(dbfile)
+    c = con.cursor()
+    c.execute("insert into report values (?)",(message,))
+    con.commit()
+    con.close()
 
 # LINE Notify
-endpoint = 'https://notify-api.line.me/api/notify'
-token = os.getenv('LINE_NOTIFY_TOKEN')
-headers = {"Authorization" : "Bearer "+ token}
+def LineNofify(message):
+    import requests
+    endpoint = 'https://notify-api.line.me/api/notify'
+    token = os.getenv('LINE_NOTIFY_TOKEN')
+    headers = {"Authorization" : "Bearer "+ token}
+    payload = {"message" :  message}
+    requests.post(endpoint ,headers = headers ,params=payload)
 
-message = nowdatetime
-payload = {"message" :  message}
-r = requests.post(endpoint ,headers = headers ,params=payload)
+def main():
+    nowdatetime = now()
+    playMp3(os.path.abspath(os.path.dirname(__file__))+"/nc100421.mp3",48000,3)
+    insertDB(os.path.abspath(os.path.dirname(__file__))+"/report.db",nowdatetime)
+    LineNofify(nowdatetime)
+
+if __name__ == '__main__':
+    main()
